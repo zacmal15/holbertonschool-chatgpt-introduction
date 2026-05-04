5e11e7496ae447570b9006149a0843b94546191a
Zac <13089@holbertonstudents.com.au>
2026-05-04 05:04:52 +0000

    Fixed system error

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,8 +5,12 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1   # 🔥 FIX: decrease n each loop
+        n -= 1
     return result
+
+if len(sys.argv) < 2:
+    print("Usage: ./factorial.py <number>")
+    sys.exit(1)
 
 f = factorial(int(sys.argv[1]))
 print(f)
